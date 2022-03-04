2d0dec319e1ba22b62cb0f32aecbd9c0aa9bfa31
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-03-04 19:24:04 +0330

    week4 E07 solve changle

---
+++ android/app/src/main/kotlin/com/example/cv_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cv_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'vazir'),
+      home: Scaffold(
+        body: SafeArea(
+          child: Text('امیراحمدادیبی'),
+        ),
+      ),
+    );
+  }
+}
