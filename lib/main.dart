fab00d675f1e9ec4fb4112b4113aa673a1b707d7
Amirahmad Adibi <amirahmadadibi@Amirahmads-MacBook-Pro.local>
2022-03-06 17:19:07 +0330

    week4 e14 textstyle and cleanups

--- lib/main.dart
+++ lib/main.dart
@@ -33,18 +33,29 @@ class Application extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(height: 20),
-          CircleAvatar(
-            backgroundImage: AssetImage('images/user.png'),
-            radius: 70,
-          ),
-          SizedBox(height: 15),
-          Text('امیراحمدام یه برنامه‌نویس و معلم'),
-          SizedBox(height: 15),
-          Text(
-              'عاشق برنامه‌نویسی موبایل اندروید و فلاتر. دوست داریم هرچیزی یادمیگرم رو یادبدم')
+          _getHeader(),
         ],
       ),
+    );
+  }
+
+  Widget _getHeader() {
+    return Column(
+      children: [
+        SizedBox(height: 20),
+        CircleAvatar(
+          backgroundImage: AssetImage('images/user.png'),
+          radius: 70,
+        ),
+        SizedBox(height: 15),
+        Text(
+          'امیراحمدام یه برنامه‌نویس و معلم',
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w900),
+        ),
+        SizedBox(height: 15),
+        Text(
+            'عاشق برنامه‌نویسی موبایل اندروید و فلاتر. دوست داریم هرچیزی یادمیگرم رو یادبدم')
+      ],
     );
   }
 }
